85e7863e3834cc74a739bebf73769f9c114dacba
kauadosantos <121907771+kauadosantos@users.noreply.github.com>
2025-10-08 16:53:35 -0300

    Inclusão do carrossel
    08/10/25.

--- home.js
+++ home.js
@@ -1,35 +1,38 @@
-let post = [];
 let listElement = document.querySelector("#Carrosel");
 
 function filmeApp() {
-  fetch(
-    "https://api.themoviedb.org/3/movie/popular?api_key=df85c4be593e70ae32d1595288534f7a&language=pt-BR"
-  )
+    fetch(
+        "https://api.themoviedb.org/3/movie/popular?api_key=df85c4be593e70ae32d1595288534f7a&language=pt-BR"
+    )
     .then((r) => r.json())
     .then((json) => {
-      post = json.results;
-
-      post.slice(0, 3).forEach((item) => {
-        let lielement = document.createElement("li");
-        let titleElement = document.createElement("h1");
-        let imgElement = document.createElement("img");
-        let buttonElement = document.createElement("button");
-
-        let titleText = document.createTextNode(item.title);
-        titleElement.appendChild(titleText);
-        
-
-        imgElement.src = `https://image.tmdb.org/t/p/w500/${item.poster_path}`;
-        lielement.appendChild(imgElement);
+        let post = json.results;
 
         
-       
+        post.slice(0, 3).forEach((item, index) => {
+            
+            let slideElement = document.createElement("div");
+            
+            slideElement.classList.add("slide");
 
-        listElement.appendChild(lielement);
-      });
+            
+            if (index === 0) {
+                slideElement.classList.add("first");
+            }
+            
+            
+            let imgElement = document.createElement("img");
+            imgElement.src = `https://image.tmdb.org/t/p/w500/${item.poster_path}`;
+
+            
+            slideElement.appendChild(imgElement);
+
+            
+            listElement.appendChild(slideElement);
+        });
     })
     .catch((error) => {
-      console.error("Deu algum erro ao buscar os filmes:", error);
+        console.error("Deu algum erro ao buscar os filmes:", error);
     });
 }
 
