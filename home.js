212e362e853a634c816dc396cff1faa9e2015f0e
kauadosantos <121907771+kauadosantos@users.noreply.github.com>
2025-10-14 16:41:44 -0300

    14/10
    ajuste de erro

--- home.js
+++ home.js
@@ -1,40 +1,31 @@
-// home.js
 let post = [];
-let listElement = document.querySelector("#Carrosel");
-
+let listElement = document.querySelector("#Filme");
+let listElementerror = document.querySelector("#FilmeTerror")
 function filmeApp(){
-    fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=df85c4be593e70ae32d1595288534f7a&language=pt-BR")
-    .then((r) => r.json())
-    .then((json) => {
-        
-        
+    fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=df85c4be593e70ae32d1595288534f7a&language=pt-BR").then((r)=> r.json()).then((json)=>{
         post = json.results;
-        
-        
-        post.slice(0, 10).forEach((item) => { 
-        
+        post.map((item)=>{
             let lielement = document.createElement("li");
             let titleElement = document.createElement("h1");
-            let imgElement = document.createElement("img");
-            let buttonElement = document.createElement("button");
+            let imgElement = document.createElement("img")
+            let buttonElement = document.createElement("button")
 
-            let titleText = document.createTextNode(item.title);
+            let titleText = document.createTextNode(item.title)
             titleElement.appendChild(titleText);
             lielement.appendChild(titleElement);
             
             
-            imgElement.src = `https://image.tmdb.org/t/p/w500/${item.poster_path}`; 
+            imgElement.src = `https://image.tmdb.org/t/p/original/${item.poster_path}`;
             lielement.appendChild(imgElement);
             
-            buttonElement.textContent = "Visualizar";
+            buttonElement.textContent = "Viziualizar"
             lielement.appendChild(buttonElement);
 
-            listElement.appendChild(lielement);
-        });
+            listElement.appendChild(lielement)
+        })
     })
-    .catch((error) => {
-        console.error("Deu algum erro ao buscar os filmes:", error);
-    });
+    .catch(()=>(
+        console.log("Deu algum erro")
+    ))
 }
-
-filmeApp();
+filmeApp()
