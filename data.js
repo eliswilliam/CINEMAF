929dad012d58c48609941a1b681a7ad8d5703b27
Letícia Mottola <leticianunes5417@gmail.com>
2025-10-21 16:30:54 -0300

    Detalhes a mais, filmes mudados, icon adicionado, correção, top 10 foi deletado.

--- data.js
+++ data.js
@@ -5,9 +5,9 @@
 window.APP_DATA = {
   hero: [
     {
-      title: 'Oppenheimer',
-      description: 'A épica história do físico J. Robert Oppenheimer e o desenvolvimento da bomba atômica.',
-      image: 'https://movies.universalpictures.com/media/04-opp-dm-mobile-banner-1080x745-pl-f01-050523-1-1-1-6458870c70c1d-1.jpg'
+      title: 'Uma Batalha Após A Outra',
+      description: 'Quando seu antigo inimigo ressurge após 16 anos, um grupo de ex-revolucionários se reúne para resgatar a filha de um dos seus membros.',
+      image: 'https://mistv.com.br/wp-content/uploads/2025/09/Banner-Site-Uma-Batalha-Apos-a-Outra.jpg'
     },
     {
       title: 'Peaky Blinders',
@@ -15,15 +15,15 @@ window.APP_DATA = {
       image: 'https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/02/Peaky-Blinders-Cast-and-Character-Guide.jpg'
     },
     {
-      title: 'Vingadores: Guerra Infinita',
-      description: 'Os Vingadores enfrentam Thanos em uma batalha pelo destino do universo.',
-      image: 'https://image.tmdb.org/t/p/original/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg'
+      title: 'Caramelo',
+      description: 'Pedro é um obstinado chef de cozinha que está prestes a realizar seu sonho de liderar um restaurante quando um diagnóstico inesperado revira a sua vida. Com a ajuda de um simpático vira-lata caramelo, ele embarca em uma emocionante jornada de redescoberta, encontrando significado e inspiração no agora.',
+      image: 'https://burnbook.com.br/wp-content/uploads/2025/08/PT-BR_CARAMELO_Main-Key-Art_Dupla_Vertical_4x5_sRGB_PRE-1.jpg'
     }
   ],
   sections: [
     {
       id: 'trending',
-      title: 'Em Alta',
+      title: 'Filmes Em Alta',
       items: [
         { title: 'Avatar: O Caminho da Água', year: '2022', rating: '7.6', image: 'https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg' },
         { title: 'Top Gun: Maverick', year: '2022', rating: '8.3', image: 'https://image.tmdb.org/t/p/w500/62HCnUTziyWcpDaBO2i1DX17ljH.jpg' },
@@ -37,21 +37,29 @@ window.APP_DATA = {
       title: 'Mais Bem Avaliados',
       items: [
         { title: 'O Senhor dos Anéis: O Retorno do Rei', year: '2003', rating: '9.0', image: 'https://image.tmdb.org/t/p/w500/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg' },
-        { title: 'Batman: O Cavaleiro das Trevas', year: '2008', rating: '9.0', image: 'https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg' },
+        { title: 'Batman: O Cavaleiro das Trevas', year: '2008', rating: '9.1', image: 'https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg' },
         { title: 'Duna', year: '2021', rating: '8.0', image: 'https://image.tmdb.org/t/p/w500/d5NXSklXo0qyIYkgV94XAgMIckC.jpg' },
         { title: 'Parasita', year: '2019', rating: '8.5', image: 'https://image.tmdb.org/t/p/w500/igw938inb6Fy0YVcwIyxQ7Lu5FO.jpg' },
-        { title: 'Interestelar', year: '2014', rating: '8.6', image: 'https://image.tmdb.org/t/p/w500/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg' }
+        { title: 'Interestelar', year: '2014', rating: '8.6', image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/6ricSDD83BClJsFdGB6x7cM0MFQ.jpg' },
+        { title: 'Um Sonho de Liberdade', year: '1994', rating: '9.3', image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/6GZBRtAhzkSQcKfkZqmrG3cdfRQ.jpg' },
+        { title: 'O Poderoso Chefão', year: '1972', rating: '9.2', image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/u8LAG1JI57U9p0s8TyEEeoykR5d.jpg' },
+        { title: 'A Viagem de Chihiro', year: '2003', rating: '8.6', image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/hhoKhsyJ3hFaxEm5pMdZRiTu2lJ.jpg' },
+        { title: '12 Homens e uma Sentença', year: '1957', rating: '9.0', image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/aiHIhV8c45FzIlbqYlswrrNyQD6.jpg' },
+        { title: 'Pulp Fiction: Tempo de Violência', year: '1994', rating: '8.8', image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/tptjnB2LDbuUWya9Cx5sQtv5hqb.jpg' },
+
       ]
     },
     {
       id: 'upcoming',
       title: 'Em Breve',
       items: [
-        { title: 'Duna: Parte Dois', year: '2024', rating: '—', image: 'https://image.tmdb.org/t/p/w500/8b8R8l88Qje9dn9OE8PY05Nxl1X.jpg' },
-        { title: 'Coringa 2', year: '2024', rating: '—', image: 'https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg' },
-        { title: 'Gladiador II', year: '2024', rating: '—', image: 'https://image.tmdb.org/t/p/w500/vPVxF6hQ5e0MyoVo2QbY4Z1cdqG.jpg' },
-        { title: 'Deadpool 3', year: '2024', rating: '—', image: 'https://image.tmdb.org/t/p/w500/EnDlndEvw6Ptpp8HIwmVcHVGq4k.jpg' },
-        { title: 'Avatar 3', year: '2025', rating: '—', image: 'https://image.tmdb.org/t/p/w500/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg' }
+        { title: 'Predador: Terras Selvagens', year: '2024', rating: '—', image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/jg6lHhCMm8FE3DlZK9KgKyRUIgp.jpg' },
+        { title: 'TRON: Ares', year: '2025', rating: '—', image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/lyq8uQzxWTCIxWEKuxyQPrbVu40.jpg' },
+        { title: 'O Telefone Preto 2', year: '2025', rating: '—', image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/p3epSUdF9qSWWHTBlA3mJ0w2i2Y.jpg' },
+        { title: 'A Meia-Irmã Feia', year: '2025', rating: '—', image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/lgO9ZdNe2ikRmQbLYnSHPxgunyP.jpg' },
+        { title: 'Avatar 3', year: '2025', rating: '—', image: 'https://image.tmdb.org/t/p/w500/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg' },
+        { title: 'Frankenstein', year: '2025', rating: '—', image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/5fqmZYw3o22VoJpXx9bjQuc7ZPV.jpg' },
+        { title: 'Chainsaw Man – O Filme: Arco da Reze ', year: '2025', rating: '—', image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/2CBMtiqU90SrcGN0aopRIHmTb5E.jpg' }
       ]
     }
   ]
